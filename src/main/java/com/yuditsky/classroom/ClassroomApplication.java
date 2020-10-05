9a7b270b7b3b26bc2901aec3e1f5a90165808276
VladislavYuditsky <vladyusha_a@mail.ru>
2020-10-05 17:59:53 +0300

    Add MailSender

--- src/main/java/com/yuditsky/classroom/ClassroomApplication.java
+++ src/main/java/com/yuditsky/classroom/ClassroomApplication.java
@@ -2,7 +2,9 @@ package com.yuditsky.classroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class ClassroomApplication {
     public static void main(String[] args) {
---
+++ src/main/java/com/yuditsky/classroom/mail/MailSender.java
@@ -0,0 +1,31 @@
+package com.yuditsky.classroom.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MailSender {
+    @Value("${spring.mail.username}")
+    private String username;
+
+    private JavaMailSender mailSender;
+
+    @Autowired
+    public MailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void send(String email, String subject, String message) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setFrom(username);
+        mailMessage.setTo(email);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+
+        mailSender.send(mailMessage);
+    }
+}
