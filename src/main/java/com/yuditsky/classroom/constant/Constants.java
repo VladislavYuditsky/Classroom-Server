c83775e420832e2763339e529530321794c375eb
VladislavYuditsky <vladyusha_a@mail.ru>
2020-10-07 17:32:33 +0300

    Change dateTimePattern

--- src/main/java/com/yuditsky/classroom/constant/Constants.java
+++ src/main/java/com/yuditsky/classroom/constant/Constants.java
@@ -1,5 +1,5 @@
 package com.yuditsky.classroom.constant;
 
 public class Constants {
-    public static final String dateTimePattern = "yyyy-MM-dd HH:mm:ss.SSS";
+    public static final String dateTimePattern = "yyyy-MM-dd HH:mm:ss";
 }
