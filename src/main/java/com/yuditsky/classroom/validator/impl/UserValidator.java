7ebefaaa0540abfd589b2bcda505243a67c766d4
VladislavYuditsky <vladyusha_a@mail.ru>
2020-10-08 13:23:35 +0300

    Add empty string check in UserValidator

--- src/main/java/com/yuditsky/classroom/validator/impl/UserValidator.java
+++ src/main/java/com/yuditsky/classroom/validator/impl/UserValidator.java
@@ -4,6 +4,7 @@ import com.yuditsky.classroom.exception.InvalidEntityException;
 import com.yuditsky.classroom.model.User;
 import com.yuditsky.classroom.validator.EntityValidator;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,24 +20,23 @@ public class UserValidator implements EntityValidator<User> {
     }
 
     public void validateEmail(String email) {
-        if (!isValidEmail(email)) {
+        if (!isValidString(emailPattern, email)) {
             throw new InvalidEntityException("Invalid email {0}", email);
         }
     }
 
     private void validateUsername(String username) {
-        if (!isValidUsername(username)) {
-            throw new InvalidEntityException("Invalid username {}", username);
+        if (!isValidString(usernamePattern, username)) {
+            throw new InvalidEntityException("Invalid username {0}", username);
         }
     }
 
-    private boolean isValidEmail(String email) {
-        Matcher matcher = emailPattern.matcher(email);
-        return matcher.matches();
-    }
-
-    private boolean isValidUsername(String username) {
-        Matcher matcher = usernamePattern.matcher(username);
-        return matcher.matches();
+    private boolean isValidString(Pattern pattern, String string){
+        if(!StringUtils.isEmpty(string)){
+            Matcher matcher = pattern.matcher(string);
+            return matcher.matches();
+        } else {
+            return false;
+        }
     }
 }
